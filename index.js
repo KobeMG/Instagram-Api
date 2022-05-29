ce9e8442f41aa799fd2d0b890f1e8ba6d438d5e7
KobeMG <kobemoya@gmail.com>
2022-05-28 19:36:21 -0600

    test

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/posting', (req, res) => {
    // res.send({ received: true, message: '¡Pokemon recibido! Podrás revisarlo en la cuenta @pokemonwebapp en Instagram.' });
     res.send({ received: true, message: 'Entrenador: '+ trainerName + ' Pokemon: ' + pokemonID });
    getPokemonImage(pokemonID);
-   fetchPokemonStats(pokemonID);
+  // fetchPokemonStats(pokemonID);
 
     // //Subiendo pokemon a instagram
     // client.login().then(() => {  //Es necesario ingresar a la cuenta de instagram primero
